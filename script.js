e44b40735486bc8fc8837985840e894535861679
santicano <canosantiago404@gmail.com>
2023-04-30 23:46:20 -0300

    Añadido click

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
+const gameController = (() => {
+    const playerX = Player('X');
+    const playerO = Player('O');
+    let round = 1;
+    let isOver = false;
+
+    const playRound = (fieldIndex) => {
+        gameBoard.setField(fieldIndex, getCurrentPlayerSign());
+        
+    }
+
+    const getCurrentPlayerSign = () => {
+        return round % 2 === 1 ? playerX.getSign() : playerO.getSign();
+    }
+})
+
 const gameBoard = (() => {
     const board = ["", "", "", "", "", "", "", "", "", ];
 
-    return { board };
+    const setField = (index, sign) => {
+        if (index > board.length) return;
+        board[index] = sign;
+    };
+
+    const getField = (index) => {
+        if (index > board.length) return;
+        return board[index];
+    };
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = '';
+        }
+    };
+
+    return { setField, getField, reset };
 })();
 
 const Player = (sign) => {
@@ -17,6 +49,8 @@ const Player = (sign) => {
 const displayController = (() => {
     const fieldElements = document.querySelectorAll(".field");
 
-    fieldElements.forEach((field) => 
-    field.textContent = 'x');
+    fieldElements.forEach((field) =>
+    field.addEventListener('click', (e) => {
+        e.target.textContent = 'x';
+    }))
 })();
